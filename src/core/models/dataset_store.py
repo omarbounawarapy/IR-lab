580e23e63779fa331c73359cd63ec6422ac80012
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-07-08 19:49:21 +0100

    feat: introduce dataset abstractions and experiment execution scaffolding

---
+++ src/core/models/dataset_store.py
@@ -0,0 +1,3 @@
+class DatasetStore : 
+    def __init__(self,registery):
+        self._registery = registery
---
+++ src/loaders/toy_loader.py
@@ -0,0 +1,5 @@
+from .base_loader import BaseLoader
+
+class ToyLoader(BaseLoader): 
+    def load(self):
+        
