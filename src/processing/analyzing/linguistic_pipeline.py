bb4f6a5b71ec43f51054c87629615bd64ef08cc1
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-07-14 14:55:00 +0000

    refactor : edited the linguistic pipeline using the new abstractions

--- src/processing/analyzing/linguistic_pipeline.py
+++ src/processing/analyzing/linguistic_pipeline.py
@@ -1,4 +1,5 @@
 from .registery import tokenizers, normalizers
+from models.documents import Document,AnalyzedDocument
 
 """
 Linguistic Pipeline for processing text through various tokenization and normalization steps.
@@ -12,8 +13,10 @@ class Linguistic_Pipeline:
     def __init__(self, steps):
         self.steps = steps
 
-    def process(self, text):
+    def process(self, document : Document) -> AnalyzedDocument:
         for (step, meta) in self.steps:
             step_cls = normalizers.get(step) or tokenizers.get(step)
-            text = step_cls(**meta)(text)
-        return text
+            content = getattr(document,"content","")
+            tokens = step_cls(**meta)(content)
+            analyzed_document = AnalyzedDocument(document,tokens)
+        return analyzed_document
