14f3253b97bb5fd5f90609f56002d0ab7f5d6c64
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-07-17 10:30:00 +0000

    added gitignore

--- src/ir_lab/models/documents/analyzed_document.py
+++ src/ir_lab/models/documents/analyzed_document.py
@@ -5,3 +5,6 @@ class AnalyzedDocument:
     def __init__(self,document : Document, tokens : list[Token]):
         self.document = document
         self.tokens = tokens
+
+    def __str__(self):
+        return f"{self.tokens=}"
---
+++ src/ir_lab/processing/analyzing/__init__.py
@@ -0,0 +1,7 @@
+from .registery import tokenizers,normalizers
+from .linguistic_pipeline import Linguistic_Pipeline
+__all__ = [
+    "tokenizers",
+    "normalizers",
+    "Linguistic_Pipeline"
+]
