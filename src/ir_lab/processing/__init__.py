020683c9654f20a8f41fef24c60acc7056d36129
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-07-20 09:15:00 +0000

    refactor : apply IR standard pipeline architecture

--- src/ir_lab/processing/__init__.py
+++
@@ -1,5 +0,0 @@
-import analyzing
-
-__all__ = [
-    "analyzing"
-]
--- src/ir_lab/processing/analyzing/__init__.py
+++
@@ -1,7 +0,0 @@
-from .registery import tokenizers,normalizers
-from .linguistic_pipeline import Linguistic_Pipeline
-__all__ = [
-    "tokenizers",
-    "normalizers",
-    "Linguistic_Pipeline"
-]
