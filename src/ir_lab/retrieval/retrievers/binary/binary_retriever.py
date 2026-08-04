11d47d2150d99815914ca67283ba3e9407f98d9a
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-04 22:02:09 +0100

    feat: added woriking intersect and union methods

--- src/ir_lab/retrieval/retrievers/binary/binary_retriever.py
+++ src/ir_lab/retrieval/retrievers/binary/binary_retriever.py
@@ -1,12 +1,23 @@
+from ir_lab.indexing.indexes import InvertedIndex
 class BinaryRetriever:
     def __init__(self, index: InvertedIndex):
         self.index = index
+        """
         self.querry_parsers={
             BooleanASTQuery: BooleanASTParser(),
             BooleanRPNQuery: BooleanRPNParser()
         }
-    
+        """
 
+    """
+    find the intersection list of two doc_id list
+    """
+    def intersect(self,docs1,docs2):
+        return docs1 & docs2
+    def union(self,docs1,docs2):
+        return docs1 | docs2
+    
+    """
     def retrieve(self, query: ExecutableQuerry ) -> set:
         parser = self.querry_parsers.get(type(query))
         if parser is None:
@@ -24,9 +35,17 @@ class BinaryRetriever:
             else : 
                 posting_list = self.index.querry(argument)
                 cache[argument] = posting_list
-                
+    """         
 
 
-if __name__ == "main" : 
-    query = "cat OR dog" 
+if __name__ == "__main__" : 
+    from ir_lab.test import Fixtures
+    index = Fixtures.inverted_index()
+    retriver = BinaryRetriever(index)
+    docs1 = index.get_term_documents("information")
+    docs2 = index.get_term_documents("model")
+
+    print(f"{retriver.intersect(docs1,docs2)=}")
+    print(f"{retriver.union(docs1,docs2)=}")
+
     
