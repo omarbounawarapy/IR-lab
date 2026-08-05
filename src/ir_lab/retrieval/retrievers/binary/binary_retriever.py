5345ec3bf9a60df89dab40a4ec462975f460a589
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-05 08:15:40 +0100

    added all binary operations

--- src/ir_lab/retrieval/retrievers/binary/binary_retriever.py
+++ src/ir_lab/retrieval/retrievers/binary/binary_retriever.py
@@ -13,9 +13,74 @@ class BinaryRetriever:
     find the intersection list of two doc_id list
     """
     def intersect(self,docs1,docs2):
-        return docs1 & docs2
-    def union(self,docs1,docs2):
-        return docs1 | docs2
+        i,j=0,0
+        results=[]
+        while i<len(docs1) and j<len(docs2):
+            if docs1[i]>docs2[j]:
+                j+=1
+            elif docs1[i]< docs2[j]:
+                i+=1
+            else :
+                results.append(docs1[i])
+                i+=1
+                j+=1
+        return results
+
+    
+    def union(self, docs1, docs2):
+        i = j = 0
+        result = []
+
+        while i < len(docs1) and j < len(docs2):
+            if docs1[i] < docs2[j]:
+                result.append(docs1[i])
+                i += 1
+            elif docs1[i] > docs2[j]:
+                result.append(docs2[j])
+                j += 1
+            else:
+                result.append(docs1[i])
+                i += 1
+                j += 1
+
+        result.extend(docs1[i:])
+        result.extend(docs2[j:])
+
+        return result
+    
+    def complement(self,docs1):
+        i,j=0,0
+        results=[]
+        docs2 = [i for i in range(self.index.n_doc+1)]
+        while i<len(docs1) and j<len(docs2):
+            if docs1[i]>docs2[j]:
+                results.append(docs2[j])
+                j+=1
+            elif docs1[i]< docs2[j]:
+                i+=1
+            else :
+                i+=1
+                j+=1
+        results.extend(docs2[j:])
+        
+        return results
+    
+    def and_not(self,docs1,docs2):
+        i,j=0,0
+        results=[]
+        while i<len(docs1) and j<len(docs2):
+            if docs1[i]>docs2[j]:
+                j+=1
+            elif docs1[i]< docs2[j]:
+                results.append(docs1[i])
+                i+=1
+            else :
+                i+=1
+                j+=1
+        results.extend(docs1[i:])
+        
+        return results
+
     
     """
     def retrieve(self, query: ExecutableQuerry ) -> set:
@@ -44,8 +109,12 @@ if __name__ == "__main__" :
     retriver = BinaryRetriever(index)
     docs1 = index.get_term_documents("information")
     docs2 = index.get_term_documents("model")
-
+    print(f"{retriver.index=}")
+    
     print(f"{retriver.intersect(docs1,docs2)=}")
     print(f"{retriver.union(docs1,docs2)=}")
+    print(f"{retriver.complement(docs1)=}")
+    print(f"{retriver.and_not(docs1,docs2)=}")
+
 
     
