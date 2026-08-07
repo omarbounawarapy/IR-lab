020e5c645aba08515bb8b881c9dc4ec106b3b80e
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-07 10:30:00 +0000

    chore : updated __init__

--- src/ir_lab/models/queries/__init__.py
+++ src/ir_lab/models/queries/__init__.py
@@ -1,6 +1,10 @@
 from .query import Query
 from .analyzed_query import AnalyzedQuery
+from .excutable_query import ExecutableQuery
+from .boolean_rpn_query import BooleanRPNQuery
 __all__ = [
     "Query",
     "AnalyzedQuery",
+    "ExecutableQuery",
+    "BooleanRPNQuery"
 ]
