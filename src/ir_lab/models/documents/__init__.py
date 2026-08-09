41641ddb465e8bc70f3ed3c2062ccf34209ddebf
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-09 01:36:18 +0100

    feat : added scored document class

--- src/ir_lab/models/documents/__init__.py
+++ src/ir_lab/models/documents/__init__.py
@@ -1,6 +1,9 @@
 from .document import Document 
 from .analyzed_document import AnalyzedDocument
+from .scored_document import ScoredDocument
+
 __all__ = [
     "Document",
-    "AnalyzedDocument"
+    "AnalyzedDocument",
+    "ScoredDocument"
 ]
