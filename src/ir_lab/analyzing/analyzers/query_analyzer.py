59ec259519c48d35ba66cf50cf2b70f60432ebad
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-10 13:20:00 +0000

    refactor : analyzer change rpn query stack in place

--- src/ir_lab/analyzing/analyzers/query_analyzer.py
+++ src/ir_lab/analyzing/analyzers/query_analyzer.py
@@ -1,5 +1,8 @@
 from .analyzer import Analyzer
 from ir_lab.models.queries import AnalyzedQuery,Query
+from ir_lab.models.tokens import Token
+from ir_lab.retrieval.parsing import Fragment
+
 
 
 class QueryAnalyzer:
@@ -7,26 +10,23 @@ class QueryAnalyzer:
     def __init__(self, analyzer : Analyzer):
         self.analyzer = analyzer
 
-    def analyze(self, query:Query) -> AnalyzedQuery:
-        analysis_results = self.analyzer.analyze(query.content)
-        tokens = getattr(analysis_results,"tokens",[])
-
-        return AnalyzedQuery(
-            query=query,
-            tokens=tokens
-        )
+    def analyze(self, rpnquery:list[Fragment]):
+        for fragment in rpnquery : 
+            if fragment.type != "OP" : 
+              analysis_results = self.analyzer.analyze(fragment.content)
+              fragment.content = analysis_results.tokens
 
 if __name__ == "__main__" : 
     from ir_lab.test import Fixtures
     from .analyzer_builder import AnalyzerBuilder
     config = Fixtures.analyzer_config()
-    query = Fixtures.query()
+    query = [Fragment(type='TERM', content='information.retrieval'), Fragment(type='TERM', content='cookies'), Fragment(type='OP', content='and')]
     builder = AnalyzerBuilder()
     analyzer = builder.build(config)
     doc_analyzer = QueryAnalyzer(analyzer=analyzer)
-    analyzed_query = doc_analyzer.analyze(query)
-    print(analyzed_query)
-    for token in analyzed_query.tokens:
-        print(f"{token=}")
+    doc_analyzer.analyze(query)
+    for fragment in query: 
+        print(fragment)
+
 
     
