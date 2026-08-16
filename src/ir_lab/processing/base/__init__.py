438432910557c8dad23e07970906871ee6001ad0
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-16 11:18:10 +0100

    feat : added init file for processing submodule

---
+++ src/ir_lab/processing/base/__init__.py
@@ -0,0 +1,14 @@
+from .base_evaluator import BaseEvaluator
+from .base_mapper import BaseMapper
+from .base_parser import BaseParser
+from .base_retriever import BaseRetriver
+from .ast.ast_builder import ASTBuilder
+
+
+all = [
+    "BaseEvaluator",
+    "BaseMapper",
+    "BaseParser",
+    "BaseRetriver",
+    "ASTBuilder"
+]
