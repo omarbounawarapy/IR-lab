92da5730a84f8c892eb8d563a1f2002e0fcabf22
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-23 09:15:00 +0000

    refactor : changed class name Retriever -> BaseRetriever

--- src/ir_lab/processing/base/base_retriever.py
+++ src/ir_lab/processing/base/base_retriever.py
@@ -1,15 +1,11 @@
 
-from abc import ABC,abstractmethod
+from abc import ABC
 
-from ir_lab.models.queries import ExecutableQuerry
 from ir_lab.indexing.indexes import BaseIndex
-from ir_lab.models.documents import ScoredDocument
 
-class Retriver(ABC):
+class BaseRetriver(ABC):
     def __init__(self, index: BaseIndex):
         self.index = index
 
 
-    @abstractmethod
-    def retrieve(self, query: ExecutableQuerry  ) -> list[ScoredDocument]:
-        pass
+  
