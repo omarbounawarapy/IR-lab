f2562bb7a0ccd14cdb17e774eab9b8d7a08fa061
omarbounawarapy <omar.bounawara.py@gmail.com>
2026-08-23 10:30:00 +0000

    refactor : renamed Evaluator into BaseEvaluator

---
+++ src/ir_lab/processing/base/base_evaluator.py
@@ -0,0 +1,48 @@
+from abc import abstractmethod
+from .ast.nodes import *
+
+class BaseEvaluator:
+
+    def __init__(self, retriever):
+        self.retriever = retriever 
+
+    def __call__(self,tree):
+
+        return self.evaluate(tree.root())
+
+    def evaluate(self, node):
+
+        if isinstance(node, TermNode):
+            return node.content
+        
+
+        if isinstance(node, BinaryNode):
+            left = self.evaluate(node.left)
+            right = self.evaluate(node.right)
+
+            return self.evaluate_binary(
+                node.operator,
+                left,
+                right
+            )
+        elif isinstance(node,UnaryNode): 
+            operand = self.evaluate(node.operand)
+
+            return self.evaluate_unary(
+                node.operator,
+                operand
+            )
+
+        raise TypeError(f"Unsupported node: {type(node)}")
+
+
+
+    @abstractmethod
+    def evaluate_binary(self, operator, left, right):
+        pass 
+
+    @abstractmethod 
+    def evaluate_unary(self,operator,operand) : 
+        pass 
+
+
